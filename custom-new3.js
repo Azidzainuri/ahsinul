69aa74e35a504cf440a3d619bcd8c178c4c64ebd
Azidzainuri <zainuriazid@gmail.com>
2022-10-05 08:56:08 +0700

    Update and rename custom-new2.js to custom-new3.js

---
+++ custom-new3.js
@@ -0,0 +1,267 @@
+// JS Link
+var linktopupAhsinul = "https://jgjk.mobi/act/topup";
+var linkwithdrawAhsinul = "https://jgjk.mobi/act/withdraw";
+var linkpointsAhsinul = "action://act/customer_points";
+var linkrideAhsinul = "#";
+var linkcarAhsinul = "#";
+var linkfoodAhsinul = "https://jgjk.mobi/m/61bb7711a42fc";
+var linksendAhsinul = "#";
+var linkshopAhsinul = "https://jgjk.mobi/m/61c25c8b611a7";
+var linklaundryAhsinul = "https://jgjk.mobi/m/61c8623c1435d";
+var linkpulsaAhsinul = "#";
+var linkmoreAhsinul = "#";
+var linkwhatsappAhsinul = "https://wa.me/6283125227588";
+var linkvoucherAhsinul = "https://jgjk.mobi/p/61daf46038aaa";
+var linktukarAhsinulAhsinul = "https://jgjk.mobi/p/61daf46038aaa";
+var linkverifemailAhsinul = "#";
+var linkInboxAhsinul = "https://jgjk.mobi/act/admin";
+var linkSaldoAhsinul = "action://act/balance";
+var linkVoucherAhsinul = "https://jgjk.mobi/m/628fda3670377";
+var linkReferralAhsinul = "action://act/referral_list";
+var linkPartnerAhsinul = "https://jgjk.mobi/act/partner";
+var linkDriverAhsinul = "https://jgjk.mobi/act/driver";
+var linkPasswordAhasinul = "action://act/password";
+
+    $(".linktopupAhsinul").click(function () {
+        window.location.href = linktopupAhsinul;
+		$(this).find("a").attr("href",linktopupAhsinul);
+		$(this).parents(".fflexx-container-buttonlink").find("a").attr("href",linktopupAhsinul);
+    });
+	$(".linkwithdrawAhsinul").click(function () {
+        window.location.href = linkwithdrawAhsinul;
+		$(this).find("a").attr("href",linkwithdrawAhsinul);
+		$(this).parents(".fflexx-container-buttonlink").find("a").attr("href",linkwithdrawAhsinul);
+    });
+	$(".linkpointsAhsinul").click(function () {
+        window.location.href = linkpointsAhsinul;
+		$(this).find("a").attr("href",linkpointsAhsinul);
+		$(this).parents(".fflexx-container-buttonlink").find("a").attr("href",linkpointsAhsinul);
+    });
+	$(".linkrideAhsinul").click(function () {
+        window.location.href = linkrideAhsinul;
+		$(this).find("a").attr("href",linkrideAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkrideAhsinul);
+    });
+	$(".linkcarAhsinul").click(function () {
+        window.location.href = linkcarAhsinul;
+		$(this).find("a").attr("href",linkcarAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkcarAhsinul);
+    });
+	$(".linkfoodAhsinul").click(function () {
+        window.location.href = linkfoodAhsinul;
+		$(this).find("a").attr("href",linkfoodAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkfoodAhsinul);
+    });
+	$(".linksendAhsinul").click(function () {
+        window.location.href = linksendAhsinul;
+		$(this).find("a").attr("href",linksendAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linksendAhsinul);
+    });
+	$(".linkshopAhsinul").click(function () {
+        window.location.href = linkshopAhsinul;
+		$(this).find("a").attr("href",linkshopAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkshopAhsinul);
+    });
+	$(".linklaundryAhsinul).click(function () {
+        window.location.href = linklaundryAhsinul;
+		$(this).find("a").attr("href",linklaundryAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linklaundryAhsinul);
+    });
+	$(".linkpulsaAhsinul").click(function () {
+        window.location.href = linkpulsaAhsinul;
+		$(this).find("a").attr("href",linkpulsaAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkpulsaAhsinul);
+    });
+	$(".linkmoreAhsinul").click(function () {
+        window.location.href = linkmoreAhsinul;
+		$(this).find("a").attr("href",linkmoreAhsinul);
+		$(this).parents(".fflexx-container").find("a").attr("href",linkmoreAhsinul);
+    });
+	$(".linkwhatsappAhsinul").click(function () {
+        window.location.href = linkwhatsappAhsinul;
+		$(this).find("a").attr("href",linkwhatsappAhsinul);
+    });
+	$(".linkvoucherAhsinul").click(function () {
+        window.location.href = linkvoucherAhsinul;
+		$(this).find("a").attr("href",linkvoucherAhsinul);
+    });
+	$(".linktukarAhsinulAhsinul").click(function () {
+        window.location.href = linktukarAhsinulAhsinul;
+		$(this).find("a").attr("href",linktukarAhsinulAhsinul);
+    });
+	$(".linkverifemailAhsinul").click(function () {
+        window.location.href = linkverifemailAhsinul;
+		$(this).find("a").attr("href",linkverifemailAhsinul);
+    });
+	$(".navlink-inbox-ahsinul").click(function () {
+        window.location.href = linkInboxAhsinul;
+		$(this).attr("href",linkInboxAhsinul);
+    });
+	$(".navlink-inbox-ahsinul").click(function () {
+        window.location.href = linkSaldoAhsinul;
+		$(this).attr("href",linkSaldoAhsinul);
+    });
+	$(".navlink-voucher-ahsinul").click(function () {
+        window.location.href = linkVoucherAhsinul;
+		$(this).attr("href",linkVoucherAhsinul);
+    });
+	$(".navlink-referral-ahsinul").click(function () {
+        window.location.href = navlink-referral-ahsinul;
+		$(this).attr("href",navlink-referral-ahsinul);
+    });
+	$(".navlink-partner-ahsinul").click(function () {
+        window.location.href = linkPartnerAhsinul;
+		$(this).attr("href",linkPartnerAhsinul);
+    });
+	$(".navlink-driver-ahsinul").click(function () {
+        window.location.href = linkDriverAhsinul;
+		$(this).attr("href",linkDriverAhsinul);
+    });	
+	$(".navlink-password-ahsinul").click(function () {
+        window.location.href = linkPasswordAhasinul;
+		$(this).attr("href",linkPasswordAhasinul);
+    });		
+	
+// JS Tabs
+let tabss = document.querySelector(".tabss");
+let tabHeader = tabss.querySelector(".tab-header");
+let tabBody = tabss.querySelector(".tab-body");
+
+let tabHeaderNodes = tabss.querySelectorAll(".tab-header > div");
+let tabBodyNodes = tabss.querySelectorAll(".tab-body > div");
+
+for(let i=0;i<tabHeaderNodes.length;i++){
+  tabHeaderNodes[i].addEventListener("click",function(){
+    tabHeader.querySelector(".activeee").classList.remove("activeee");
+    tabHeaderNodes[i].classList.add("activeee");
+    tabBody.querySelector(".activeee").classList.remove("activeee");
+    tabBodyNodes[i].classList.add("activeee");
+    tabIndicator.style.left = `calc(calc(calc(25% - 5px) * ${i}) + 10px)`;
+  });
+}
+
+// JS Menu Button
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+
+// JS Humberger
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.add("open");
+});
+
+close.addEventListener("click", () => {
+  navList.classList.remove("open");
+});
+
+// Colors
+const widget = document.querySelector(".widget");
+const control = document.querySelector(".control");
+
+widget.addEventListener("click", () => {
+  control.classList.toggle("open");
+});
+
+const colors = [...document.querySelectorAll(".colors span")];
+document.querySelector(":root").style.setProperty("--customColor", "#FF670D");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    const currentColor = color.dataset.id;
+    document
+      .querySelector(":root")
+      .style.setProperty("--customColor", currentColor);
+  });
+});
+
+window.addEventListener("scroll", () => {
+  control.classList.remove("open");
+});
+
+ // JS Copy
+ function copy(copyId){
+    let inputElement = document.createElement("input");
+    inputElement.type = "text";
+    let copyText = document.getElementById(copyId).innerHTML;
+    inputElement.value = copyText;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputElement);
+    
+    document.getElementById("alerttt").style.display = "block";
+    setTimeout(function(){
+        document.getElementById("alerttt").style.display = "none";
+    }, 1000);
+}
+
+ // JS Chat Whatsapp
+ $(document).on("click", "#send-it", function () {
+    var a = document.getElementById("chat-input");
+    if ("" != a.value) {
+        var b = $("#get-number").text(),
+            c = document.getElementById("chat-input").value,
+            d = "https://api.whatsapp.com/send",
+            e = b,
+            f = "&text=" + c;
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) var d = "whatsapp://send";
+        var g = d + "?phone=" + e + f;
+        window.open(g, "_blank");
+    }
+}),
+    $(document).on("click", ".informasi", function () {
+        (document.getElementById("get-number").innerHTML = $(this).children(".my-number").text()),
+            $(".start-chat,.get-new").addClass("show").removeClass("hide"),
+            $(".home-chat,.head-home").addClass("hide").removeClass("show"),
+            (document.getElementById("get-nama").innerHTML = $(this).children(".info-chat").children(".chat-nama").text()),
+            (document.getElementById("get-label").innerHTML = $(this).children(".info-chat").children(".chat-label").text());
+    }),
+    $(document).on("click", ".close-chat", function () {
+        $("#whatsapp-chat").addClass("hide").removeClass("show");
+    }),
+    $(document).on("click", ".blantershow-chat", function () {
+        $("#whatsapp-chat").addClass("show").removeClass("hide");
+    });
+	
+// Nav Link	
+ $(".nav__link").on("click", function () {
+  if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") || location.hostname == this.hostname) {
+   $(this).parents(".nav__list").find("a").removeClass("active-link");
+    $(this).addClass("active-link");
+    $("#achsinhome, #achsinaktivitas, #achsinaccount").hide();
+    var b = $(this.hash);
+    if (b.length) {
+      var a = $(this).attr("href");
+      $(a).fadeIn();
+      return false;
+    }
+  }
+});
+$(".nav__list").each(function () {
+  $(this).find("a:first").addClass("active-link");
+  $(this).find("a:first").trigger("click");
+});
